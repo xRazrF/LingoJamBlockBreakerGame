463c97cce95fdd4e5e629f17caeb1755ea2433cf
Naufal Rizky Rahardian <naufalrizky.r@gmail.com>
2021-09-16 11:10:57 +0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,217 @@
+function checkKey(e) {
+	function moveBar (a, b) {
+		game[barVertical][bar[a] + b] = "▀";
+		game[barVertical][bar[a]] = "░";
+		bar[a] += b;
+	}
+	e = e || window.event;
+	if (e.keyCode === 37 && playState !== "pause") {
+		if (game[barVertical][bar[barFirst] - 1] !== "█") {
+			if (playState !== "play") {
+				game[ballVertical][ballHorizontal] = "░";
+				game[ballVertical][ballHorizontal + -1] = "●";
+				ballHorizontal += -1;
+			}
+			for (let i = barFirst; i <= barLength; i++){
+				moveBar(i, -1);
+			}
+		}
+		updatePosition ();
+	}
+	else if (e.keyCode === 39 && playState !== "pause") {
+		if (game[barVertical][bar[barLength] + 1] !== "█") {
+			if (playState !== "play") {
+				game[ballVertical][ballHorizontal] = "░";
+				game[ballVertical][ballHorizontal + 1] = "●";
+				ballHorizontal += 1;
+			}
+			for (let i = barLength; i >= barFirst; i--){
+				moveBar(i, 1);
+			}
+		}
+		updatePosition ();
+	}
+	if (e.keyCode === 32) {
+		const ballSlowness = 100;
+		e.preventDefault();
+		if (playState === "stop" || playState === "break") {
+			moveVertical = -1;
+			moveHorizontal = randomDirection();
+			playState = "play";
+			gameInterval = window.setInterval(function(){
+				ballMove ();
+			}, ballSlowness);
+		}
+		else if (playState === "play") {
+			clearInterval(gameInterval);
+			$("#score").html("Paused");
+			playState = "pause";
+		}
+		else if  (playState === "pause") {
+			gameInterval = window.setInterval(function(){
+				ballMove ();
+			}, ballSlowness);
+			setScore(score);
+			playState = "play";
+		}
+	}
+}
+function ballMove () {
+	function scoring() {
+		score += 10;
+		setScore(score);
+	}
+	if (game[ballVertical][ballHorizontal + moveHorizontal] === "▓") {
+		game[ballVertical][ballHorizontal + moveHorizontal] = "░";
+		moveHorizontal *= -1;
+		scoring();
+	}
+	if (game[ballVertical + moveVertical][ballHorizontal] === "▓") {
+		game[ballVertical + moveVertical][ballHorizontal] = "░";
+		moveVertical *= -1;
+		 scoring();
+	}
+	else if (game[ballVertical + moveVertical][ballHorizontal + moveHorizontal] === "▓") {
+		game[ballVertical + moveVertical][ballHorizontal + moveHorizontal] = "░";
+		moveVertical *= -1;
+		scoring();
+	}
+	if (game[ballVertical][ballHorizontal + moveHorizontal] === "█") {
+		moveHorizontal *= -1;
+	}
+	if (game[ballVertical + moveVertical][ballHorizontal] === "█") {
+		moveVertical *= -1;
+	}
+	else if (game[ballVertical + moveVertical][ballHorizontal + moveHorizontal] === "█") {
+		moveVertical *= -1;
+	}
+	if (game[ballVertical + moveVertical][ballHorizontal + moveHorizontal] === "▀") {
+		if (game[ballVertical + moveVertical][ballHorizontal + moveHorizontal - 1] === "▀" && game[ballVertical + moveVertical][ballHorizontal + moveHorizontal + 1] === "▀"  && game[ballVertical + moveVertical][ballHorizontal + moveHorizontal - 2] === "▀" && game[ballVertical + moveVertical][ballHorizontal + moveHorizontal + 2] === "▀") {
+			game[ballVertical ][ballHorizontal ] = "░";
+			ballHorizontal += randomDirection();
+			game[ballVertical ][ballHorizontal ] = "●";
+		}
+		moveHorizontal = randomDirection();
+		moveVertical *= -1;
+		if (game[ballVertical + moveVertical][ballHorizontal + moveHorizontal] === "█") {
+			moveHorizontal *= -1
+		}
+	}
+	if (ballVertical === barVertical) {
+		clearInterval(gameInterval);
+		if (life === 0) {
+			playState = "stop";
+			const gameOver = "Game Over!\nYour score: " + score + " pts";
+			alert(gameOver);
+			console.log(gameOver);
+		}
+		else {
+			playState = "break";
+			life -= 1;
+			setLife(life);
+		}
+		reset();
+		return;
+	}
+	game[ballVertical + moveVertical][ballHorizontal + moveHorizontal] = "●";
+	game[ballVertical][ballHorizontal] = "░";
+	ballVertical += moveVertical;
+	ballHorizontal += moveHorizontal;
+	updatePosition();
+}
+function updatePosition() {
+	$("#game").html(game.map(a => a.join('')).join('<br>'));
+}
+function randomDirection() {
+	const directions = [1, -1];
+	const random = directions[Math.floor(Math.random() * directions.length)];
+	return random;
+}
+function setLife(life) {
+	let lifeString = "";
+	for (let i = 0; i < life; i++) {
+		lifeString += "♥";
+	}
+	if (life === 0) {
+		lifeString = "<br>";
+	}
+	$("#life").html(lifeString);
+}
+function setScore(score) {
+	let lifeScore = "Score: " + score + " pts";
+	$("#score").html(lifeScore);
+}
+function reset() {
+	if (playState === "stop") {
+		game = JSON.parse(JSON.stringify(originalGame));
+		score = 0;
+		life = 3;
+		setScore(score);
+		setLife(life);
+	}
+	game[ballVertical][ballHorizontal] = "░";
+	ballVertical = barVertical - 1;
+	ballHorizontal = (game[ballVertical].length/2)-(1/2);
+	game[ballVertical][ballHorizontal] = "●";
+	for (let i = barFirst; i <= barLength; i++) {
+		game[barVertical][bar[i]] = "░";
+	}
+	bar = [barHorizontal-3, barHorizontal-2, barHorizontal-1, barHorizontal , barHorizontal+1, barHorizontal+2, barHorizontal+3];
+	for (let i = barFirst; i <= barLength; i++) {
+		game[barVertical][bar[i]] = "▀";
+	}
+	updatePosition();
+}
+function init() {
+	playState = "stop";
+	originalGame = JSON.parse(JSON.stringify(game));
+	bar = [barHorizontal-3, barHorizontal-2, barHorizontal-1, barHorizontal , barHorizontal+1, barHorizontal+2, barHorizontal+3];
+	barVertical = game.length-2;
+	barHorizontal = (game[barVertical].length/2)-(1/2);
+	barFirst = 0;
+	barLength = bar.length - 1;
+	ballVertical = barVertical-1;
+	ballHorizontal = (game[ballVertical].length/2)-(1/2);
+	const initialization = '' +
+	'<b id="gameArea" style="text-align: center; font-family: Courier New; line-height: 100%;">' +
+		'<p id="life"><p id="score"></p><p id="game"></p>' +
+	'</b>';
+	$(".main-title").after(initialization);
+	$(document).keydown(checkKey);
+	reset();
+}
+let game = [];
+game.push(["█", "█", "█", "█", "█", "█", "█", "█","█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█","█", "█", "█", "█", "█", "█", "█", "█", "█"]);
+game.push(["█", "█", "▓", "▓","▓", "▓","▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "█", "█"]);
+game.push(["█", "█", "▓", "▓","▓", "▓","▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "█", "█"]);
+game.push(["█", "█", "▓", "▓","▓", "▓","▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "█", "█"]);
+game.push(["█", "█", "▓", "▓","▓", "▓","▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "█", "█"]);
+game.push(["█", "█", "▓", "▓","▓", "▓","▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "█", "█"]);
+game.push(["█", "█", "▓", "▓","▓", "▓","▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "█", "█"]);
+game.push(["█", "█", "▓", "▓","▓", "▓","▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓", "▓","▓", "▓", "▓", "▓", "▓", "▓", "▓", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "█", "█", "█", "█", "█", "█", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "█", "█", "█", "█", "█", "█", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░", "░","░", "░", "░", "░", "░", "░", "░", "█", "█"]);
+game.push(["█", "█", "█", "█", "█", "█", "█", "█","█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█", "█","█", "█", "█", "█", "█", "█", "█", "█", "█"]);
+let bar, barVertical, barHorizontal, barFirst, barLength;
+let ballVertical, ballHorizontal;
+let moveVertical, moveHorizontal;
+let playState, score, life, gameInterval, originalGame;
+init();
